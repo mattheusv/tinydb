9264941c977cb3d2a290af3b78254c542d3160d1
Matheus Alcantara <mths.dev@pm.me>
2022-07-20 19:36:50 -0300

    Add basic integration tests for INSERT and SELECT

---
+++ tests/basic_test.rs
@@ -0,0 +1,64 @@
+use std::path::PathBuf;
+
+use tinydb::{engine::Engine, initdb::init_database, storage::BufferPool};
+
+#[test]
+fn test_insert_select() {
+    let default_db_name = "tinydb";
+
+    // Create a default tinydb database.
+    init_database(&PathBuf::from("data"), &default_db_name).expect("Failed init default database");
+
+    let mut output = Vec::new();
+    let buffer = BufferPool::new(120);
+    let mut engine = Engine::new(buffer, "data");
+
+    engine
+        .exec(
+            &mut output,
+            "CREATE TABLE t(a int, b int, c int);",
+            default_db_name,
+        )
+        .expect("Error to create table");
+
+    // Add value for all columns
+    engine
+        .exec(
+            &mut output,
+            "INSERT INTO t(a, b, c) VALUES(10, 20, 30)",
+            default_db_name,
+        )
+        .expect("Error to insert data on table");
+
+    // Add NULL values for some fields
+    engine
+        .exec(
+            &mut output,
+            "INSERT INTO t(a, c) VALUES(40, 50)",
+            default_db_name,
+        )
+        .expect("Error to insert data on table");
+    engine
+        .exec(&mut output, "INSERT INTO t(b) VALUES(60)", default_db_name)
+        .expect("Error to insert data on table");
+
+    assert!(
+        output.is_empty(),
+        "Expected empty output after creating and inserting into table"
+    );
+
+    engine
+        .exec(&mut output, "SELECT * FROM t;", default_db_name)
+        .expect("Error to insert data on table");
+
+    let output =
+        std::str::from_utf8(&output.as_slice()).expect("Failed to convert output to string");
+    let expected = "  a   |  b   |  c   
+------+------+------
+  10  |  20  |  30  
+  40  | NULL |  50  
+ NULL |  60  | NULL 
+
+";
+    assert_eq!(output, expected);
+}
