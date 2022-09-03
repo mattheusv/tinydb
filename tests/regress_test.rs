b794ba130ccae9e563fef38c3605b4d90724a741
Matheus Alcantara <mths.dev@pm.me>
2022-09-03 14:37:00 -0300

    Create output file from regress tests to compare

--- tests/regress_test.rs
+++ tests/regress_test.rs
@@ -13,6 +13,7 @@ fn test_regress() {
         .expect("");
 
     let expected_path = Path::new("tests").join("regress").join("expected");
+    let output_path = Path::new("tests").join("regress").join("output");
     for sql_file in sql_entries {
         let mut buffer = BufferPool::new(120);
 
@@ -22,15 +23,14 @@ fn test_regress() {
         let mut output = Vec::new();
         let mut engine = Engine::new(buffer, "data");
 
-        let expected_sql = fs::read_to_string(expected_path.join(format!(
-            "{}.out",
-            sql_file
-                .file_name()
-                .expect("Failed to get filename from sql file")
-                .to_str()
-                .expect("Failed to get name of sql file")
-        )))
-        .expect("");
+        let sql_name = sql_file
+            .file_name()
+            .expect("Failed to get filename from sql file")
+            .to_str()
+            .expect("Failed to get name of sql file");
+
+        let expected_sql =
+            fs::read_to_string(expected_path.join(format!("{}.out", sql_name))).expect("");
 
         let sql = fs::read_to_string(&sql_file).expect("Failed to read expected sql file");
         for sql in sql.split_inclusive(";").collect::<Vec<&str>>() {
@@ -42,6 +42,8 @@ fn test_regress() {
 
         let output =
             std::str::from_utf8(&output.as_slice()).expect("Failed to convert output to string");
+
+        fs::write(output_path.join(sql_name), output).unwrap();
 
         assert_eq!(expected_sql, output);
     }
