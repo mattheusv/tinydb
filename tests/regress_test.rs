1b192ea14e5ed7a9350e28205d43bce717395bcb
Matheus Alcantara <mths.dev@pm.me>
2022-09-03 14:09:38 -0300

    Show SQL on regress test fail message

--- tests/regress_test.rs
+++ tests/regress_test.rs
@@ -35,7 +35,9 @@ fn test_regress() {
         let sql = fs::read_to_string(&sql_file).expect("Failed to read expected sql file");
         for sql in sql.split_inclusive(";").collect::<Vec<&str>>() {
             output.extend_from_slice(&sql.as_bytes().to_vec());
-            engine.exec(&mut output, &sql, &db_oid).expect("");
+            engine
+                .exec(&mut output, &sql, &db_oid)
+                .expect(&format!("failed to execute {}", sql));
         }
 
         let output =
